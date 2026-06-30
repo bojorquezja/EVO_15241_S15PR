a79a3875a3e2de9bb948f3184a1da272c4c093c2
CarolGamer <carolajen05@gmail.com>
2026-06-30 18:38:57 -0500

    Se creo la caprpeta Modelos y modelo de Usuario

---
+++ prueba/Modelos/Usuario.cs
@@ -0,0 +1,10 @@
+﻿namespace prueba.Modelos
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; } = "";
+        public string Correo { get; set; } = "";
+        public string Contraseña { get; set; } = "";
+    }
+}
